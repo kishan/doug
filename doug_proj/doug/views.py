bc2b2378f76b88f72dc3aa9c14f1877455dc64c5
Sid Malladi <scmallad@andrew.cmu.edu>
2017-02-10 23:51:22 -0500

    Completed senators' phone numbers fetch

--- doug_proj/doug/views.py
+++ doug_proj/doug/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 import requests
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hi, my name is Doug.")
 
-def senators_phone(request, address):
-    pass
+def senators_phone(address):
+    payload = {'key': 'AIzaSyBXie2DAg6CPE6YOfikEDi_Io_LBDD1h9M',
+        'address': address, 'roles': 'legislatorUpperBody'}
+    req_url = 'https://www.googleapis.com/civicinfo/v2/representatives'
+    res = requests.get(req_url, params=payload).json()
+    senators_info = res['officials']
+    senators = []
+    for senator_info in senators_info:
+        senator = {'name': senator_info['name'], 'phone': senator_info['phones'][0]}
+        senators.append(senator)
+    return HttpResponse(senators)
