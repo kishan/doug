1543fc8caae722ac475411b46d37dd82b27be24b
kishan <kspatel2018@gmail.com>
2017-02-11 02:07:39 -0500

    fix syntax

--- doug_proj/doug/urls.py
+++ doug_proj/doug/urls.py
@@ -6,3 +6,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<address>[a-z]*[0-9]+)/$', views.senators_phone, name='senators_phone'),
     url(r'^test/$', views.webhook, name='webhook'),
+]
