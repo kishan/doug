b1b327e8944b197b50a862996f30f4a119b3423a
kishan <kspatel2018@gmail.com>
2017-02-11 06:02:55 -0500

    quick format change

--- doug_proj/doug/views.py
+++ doug_proj/doug/views.py
@@ -20,8 +20,11 @@ def index(request):
     return HttpResponse("Hi, my name is Doug.")
 
 def senators_phone(address):
-    payload = {'key': 'AIzaSyBXie2DAg6CPE6YOfikEDi_Io_LBDD1h9M',
-        'address': address, 'roles': 'legislatorUpperBody'}
+    payload = {
+        'key': 'AIzaSyBXie2DAg6CPE6YOfikEDi_Io_LBDD1h9M',
+        'address': address, 
+        'roles': 'legislatorUpperBody'
+    }
     req_url = 'https://www.googleapis.com/civicinfo/v2/representatives'
     res = requests.get(req_url, params=payload).json()
     senators_info = res['officials']
