0e9f7b6e4358ad3fcd9b9bdc891ac94680294fbb
Sid Malladi <scmallad@andrew.cmu.edu>
2017-02-10 21:28:27 -0500

    Set up Django project and app

---
+++ doug_proj/doug/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<address>[a-z]*[0-9]+)/$', views.senators_phone, name='senators_phone'),
+]
