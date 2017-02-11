3d8d699b651b53a142f88844af6fbed29c1303c6
DipoChiang <dipo233@gmail.com>
2017-02-11 06:33:20 -0500

    text summarizing

--- doug_proj/doug/intent_parsing.py
+++ doug_proj/doug/intent_parsing.py
@@ -31,5 +31,19 @@ def parse_input(input, sessionId):
         print ("bot says: "+ bot_response)
 
 
+def summarize_article(url):
+    headers = {'X-AYLIEN-TextAPI-Application-Key': 'f1ceba6c3a0b24ffd26d84c0a38168a1',
+                'X-AYLIEN-TextAPI-Application-ID': 'b324016b'}
+    params = {'url': url, 
+              'sentences_number': 2}
 
-parse_input("action: economy?", 1233333)
+    r = requests.get("https://api.aylien.com/api/v1/summarize", params=params, headers=headers)
+    j = json.loads(r.text)
+
+    try:
+        return " ".join(j['sentences'])
+    except Exception as e:
+        return "Error summarizing article."
+
+# summarize_article("https://www.washingtonpost.com/news/morning-mix/wp/2017/02/08/hillary-clinton-just-said-it-but-the-future-is-female-began-as-a-1970s-lesbian-separatist-slogan/?utm_term=.05294b4940db")
+# parse_input("action: economy?", 1233333)
